b1a267f842bb1738844153059c3854b24163528f
AGgamer01 <91713329+AGgamer01@users.noreply.github.com>
2021-11-15 07:26:23 +0700

    Add files via upload

---
+++ websendiri/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Ara Akane Sakura</title>
+
+	<!--css-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<!--css-->
+
+
+</head>
+<body>
+
+	<!--Navbar-->
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="#">Profile</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" id="navbarNav">
+		      <ul class="navbar-nav">
+		        <li class="nav-item">
+		          <a class="nav-link active" aria-current="page" href="https://discord.gg/Xpn8X2bB">discord</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="https://www.youtube.com/channel/UCZBd1IVfLRx73KpYh2RD3Uw">Youtube</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="https://trakteer.id/araa-akane-sakura-ch-ibqzf">Trakteer</a>
+		        </li>
+
+		        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Media sosial
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="https://www.instagram.com/araa_akanechan/">Instagram</a></li>
+            <li><a class="dropdown-item" href="https://www.facebook.com/mutia.chan.184">Facebook</a></li>
+            <li><a class="dropdown-item" href="#">Twiter (coming soon)</a></li>
+          </ul>
+        </li>
+
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+	<!--Navbar-->
+
+	<!--logoVtuber-->
+
+		<section class="jumbotron text-center">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col">
+            <img src="https://cdn.discordapp.com/attachments/897117355941969931/906550564345757726/Tak_berjudul366_20211106212629.png" alt="kanna" width="230" class="rounded-circle img-thumbnail" data-aos="zoom-in" />
+            <h1 class="display-4 fw-bold" data-aos="zoom-in" data-aos-delay="300"><i class="bi bi-person-circle"></i> Ara Akane Sakura</h1>
+            <p class="lead fw-bold" data-aos="zoom-in" data-aos-delay="400"><i class="bi bi-terminal-fill"></i> Vtuber | Streamer | Idol | Influenser<i class="bi bi-window-sidebar"></i></p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path
+                fill="#ffffff"
+                fill-opacity="1"
+                d="M0,0L0,256L80,256L80,0L160,0L160,128L240,128L240,224L320,224L320,128L400,128L400,192L480,192L480,160L560,160L560,64L640,64L640,32L720,32L720,0L800,0L800,320L880,320L880,320L960,320L960,288L1040,288L1040,160L1120,160L1120,64L1200,64L1200,128L1280,128L1280,96L1360,96L1360,32L1440,32L1440,320L1360,320L1360,320L1280,320L1280,320L1200,320L1200,320L1120,320L1120,320L1040,320L1040,320L960,320L960,320L880,320L880,320L800,320L800,320L720,320L720,320L640,320L640,320L560,320L560,320L480,320L480,320L400,320L400,320L320,320L320,320L240,320L240,320L160,320L160,320L80,320L80,320L0,320L0,320Z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </section>
+	<!--logoVtuber-->
+
+	<!--coloum gambar-->
+
+	<div class="container">
+		<div class="row row-cols-2 row-cols-lg-4 g-5 g-lg-4.5">
+
+			<div class="col">
+				<div class="p-3 border bg-light"><a class="nav-link" href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqXtMFVlDDt1thG4-N3QUJwE4dhynH-QyIEg&usqp=CAU" width="150" height="300"></a></div>				
+			</div>
+
+			<div class="col">
+				<div class="p-3 border bg-light"><a class="nav-link" href="#"><img src="https://cdn.discordapp.com/attachments/897117355941969931/903945929118326834/Tak_berjudul317_20211030165641.jpg" width="150" height="300"></a></div>				
+			</div>
+
+			<div class="col">
+				<div class="p-3 border bg-light"><a class="nav-link" href="#"><img src="https://cdn.discordapp.com/attachments/897117355941969931/903864059118628934/Tak_berjudul314_20211030113228.jpg" width="150" height="300" class="text-center"></a></div>				
+			</div>
+
+			<div class="col">
+				<div class="p-3 border bg-light"><a class="nav-link" href="#"><img src="https://cdn.discordapp.com/attachments/897117355941969931/907147833222766592/Tak_berjudul368_20211108130020.png" width="150" height="300"></a></div>				
+			</div>
+
+			<div class="col">
+				<div class="p-3 border bg-light"><a class="nav-link" href="#"><img src="https://cdn.discordapp.com/attachments/897117355941969931/908221031930560522/Tak_berjudul386_20211111054039.png" width="150" height="300"></a></div>				
+			</div>
+
+			<div class="col">
+				<div class="p-3 border bg-light"><a class="nav-link" href="#"><img src="https://cdn.discordapp.com/attachments/897117355941969931/908234994034552872/IMG_20211110_210608.jpg" width="150" height="300"></a></div>				
+			</div>
+
+			<div class="col">
+				<div class="p-3 border bg-light"><a class="nav-link" href="#"><img src="https://cdn.discordapp.com/attachments/897117355941969931/905832693693038622/Tak_berjudul354_20211104213422.jpg" width="150" height="300"></a></div>				
+			</div>
+
+			<div class="col">
+				<div class="p-3 border bg-light"><a class="nav-link" href="#"><img src="https://cdn.discordapp.com/attachments/897117355941969931/906082625238757396/Tak_berjudul355_20211105094809.jpg" width="150" height="300"></a></div>				
+			</div>
+			
+		</div>
+	</div>
+	<!--coloum gambar-->		
+
+	<!--jscode-->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+		<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+		<script>
+  			AOS.init();
+		</script>
+	<!--jscode-->
+</body>
+</html>
